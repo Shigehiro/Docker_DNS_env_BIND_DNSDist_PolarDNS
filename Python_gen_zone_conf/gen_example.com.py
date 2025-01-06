2541d79eb97bc0a4567d702ef9d4ca37088578e1
Shigehiro Hattori <narihiro.h@gmail.com>
2025-01-06 16:01:21 +0900

    refactoring

--- Python_gen_zone_conf/gen_example.com.py
+++ Python_gen_zone_conf/gen_example.com.py
@@ -3,6 +3,7 @@
 zone_prefix = 'sub'
 zone_suffix = 'example.com'
 zone_suffix_polardns = 'broken.com'
+zone_suffix_delay = 'delay.com'
 number_of_zone = 100000
 
 # Generate zone file for example.com
@@ -73,6 +74,40 @@ ns02.{zone_prefix}{domain_number}.{zone_suffix_polardns}. IN AAAA 2001:db8:1::29
 """
         f.write(conf_string)
 
+# Generate zone file for delay.com
+with open('../bind_config/example.com/records/delay.com.template.db', 'w') as f:
+    conf_string = """
+$TTL 3600
+@ IN SOA ns01 ns01 (
+2010062303
+1h
+15m
+30d
+1h )
+  IN NS ns01
+  IN NS ns02
+
+ns01 IN A 192.168.55.24 ; authority of delay.com
+ns02 IN A 192.168.55.25 ; authority of delay.com
+ns01 IN AAAA 2001:db8:1::24 ; authority of delay.com
+ns02 IN AAAA 2001:db8:1::25 ; authority of delay.com
+"""
+    f.write(conf_string)
+
+# delegation for sub.delay.com
+with open('../bind_config/example.com/records/delay.com.template.db', 'a') as f:
+    for i in range(0,number_of_zone):
+        domain_number = str(f"{i:06}")
+        conf_string = f"""
+{zone_prefix}{domain_number}.{zone_suffix_delay}. IN NS ns01.{zone_prefix}{domain_number}.{zone_suffix_delay}.
+{zone_prefix}{domain_number}.{zone_suffix_delay}. IN NS ns02.{zone_prefix}{domain_number}.{zone_suffix_delay}.
+ns01.{zone_prefix}{domain_number}.{zone_suffix_delay}. IN A 192.168.55.28 ; authority of {zone_prefix}{domain_number}.{zone_suffix_delay}.
+ns02.{zone_prefix}{domain_number}.{zone_suffix_delay}. IN A 192.168.55.29 ; authority of {zone_prefix}{domain_number}.{zone_suffix_delay}.
+ns01.{zone_prefix}{domain_number}.{zone_suffix_delay}. IN AAAA 2001:db8:1::28 ; authority of {zone_prefix}{domain_number}.{zone_suffix_delay}.
+ns02.{zone_prefix}{domain_number}.{zone_suffix_delay}. IN AAAA 2001:db8:1::29 ; authority of {zone_prefix}{domain_number}.{zone_suffix_delay}.
+"""
+        f.write(conf_string)
+
 # Generate named.conf
 with open('../bind_config/example.com/config/named.conf', 'w') as f:
     conf_string = """
@@ -106,6 +141,14 @@ zone "{zone_suffix}" in {{
 zone "{zone_suffix_polardns}" in {{
   type master;
   file "/var/lib/bind/broken.com.template.db";
+}};
+"""
+    f.write(conf_string)
+
+    conf_string = f"""
+zone "{zone_suffix_delay}" in {{
+  type master;
+  file "/var/lib/bind/delay.com.template.db";
 }};
 """
     f.write(conf_string)
--- Python_gen_zone_conf/gen_sub.example.com.py
+++ Python_gen_zone_conf/gen_sub.example.com.py
@@ -3,6 +3,7 @@
 zone_prefix = 'sub'
 zone_suffix = 'example.com'
 zone_suffix_polardns = 'broken.com'
+zone_suffix_delay = 'delay.com'
 number_of_zone = 100000
 
 # Generate zone file for sub.example.com
@@ -41,12 +42,70 @@ ns02.{zone_prefix}{domain_number} IN AAAA 2001:db8:1::60
 """
         f.write(conf_string)
 
+# Generate zone file for sub.delay.com
+for i in range(0,number_of_zone):
+    domain_number = str(f"{i:06}")
+    with open(f'../bind_config/sub.example.com/records/sub{domain_number}.delay.com.template.db', 'w') as f:
+        conf_string = """
+$TTL 60
+@ IN SOA ns01 ns01 (
+2010062303
+1h
+15m
+30d
+1h )
+  IN NS ns01
+  IN NS ns02
+
+ns01 IN A 192.168.55.28 
+ns02 IN A 192.168.55.29
+ns01 IN AAAA 2001:db8:1::28
+ns02 IN AAAA 2001:db8:1::29
+"""
+        f.write(conf_string)
+
+# delegation for sub.delay.com
+for i in range(0,number_of_zone):
+    domain_number = str(f"{i:06}")
+    with open(f'../bind_config/sub.example.com/records/sub{domain_number}.delay.com.template.db', 'a') as f:
+        conf_string = f"""
+{zone_prefix}{domain_number} IN NS ns01.{zone_prefix}{domain_number}
+{zone_prefix}{domain_number} IN NS ns02.{zone_prefix}{domain_number}
+ns01.{zone_prefix}{domain_number} IN A 192.168.55.61
+ns02.{zone_prefix}{domain_number} IN A 192.168.55.62
+ns01.{zone_prefix}{domain_number} IN AAAA 2001:db8:1::61
+ns02.{zone_prefix}{domain_number} IN AAAA 2001:db8:1::62
+"""
+        f.write(conf_string)
+
 # Generate zone file for sub.broken.com
 for i in range(0,number_of_zone):
     domain_number = str(f"{i:06}")
     with open(f'../bind_config/sub.example.com/records/sub{domain_number}.broken.com.template.db', 'w') as f:
         conf_string = """
-$TTL 3600
+$TTL 3
+@ IN SOA ns01 ns01 (
+2010062303
+1h
+15m
+30d
+1h )
+  IN NS ns01
+  IN NS ns02
+
+ns01 IN A 192.168.55.28
+ns02 IN A 192.168.55.29
+ns01 IN AAAA 2001:db8:1::28
+ns02 IN AAAA 2001:db8:1::29
+"""
+        f.write(conf_string)
+
+# Generate zone file for sub.broken.com
+for i in range(0,number_of_zone):
+    domain_number = str(f"{i:06}")
+    with open(f'../bind_config/sub.example.com/records/sub{domain_number}.broken.com.template.db', 'w') as f:
+        conf_string = """
+$TTL 3
 @ IN SOA ns01 ns01 (
 2010062303
 1h
@@ -112,6 +171,18 @@ for i in range(0,number_of_zone):
 zone "{zone_prefix}{domain_number}.{zone_suffix}" in {{
   type master;
   file "/var/lib/bind/sub{domain_number}.example.com.template.db";
+}};
+"""
+        f.write(conf_string)
+
+# Generate named.conf for sub.delay.com
+for i in range(0,number_of_zone):
+    domain_number = str(f"{i:06}")
+    with open('../bind_config/sub.example.com/config/named.conf', 'a') as f:
+        conf_string = f"""
+zone "{zone_prefix}{domain_number}.{zone_suffix_delay}" in {{
+  type master;
+  file "/var/lib/bind/sub{domain_number}.delay.com.template.db";
 }};
 """
         f.write(conf_string)
