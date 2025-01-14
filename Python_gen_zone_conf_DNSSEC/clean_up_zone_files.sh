525a5761f4e088181b62ae11d1923df6e88aba80
Shigehiro Hattori <narihiro.h@gmail.com>
2025-01-14 16:57:03 +0900

    minor refactoring

--- Python_gen_zone_conf_DNSSEC/clean_up_zone_files.sh
+++ Python_gen_zone_conf_DNSSEC/clean_up_zone_files.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
-tree -if ../bind_config_dnssec/sub.sub.example.com/records/ |egrep 'dsset|template|.key|.private' | xargs -I{} rm {}
-tree -if ../bind_config_dnssec/sub.sub.delay.com/records/ |egrep 'dsset|template|.key|.private' | xargs -I{} rm {}
+rm -rf ../bind_config_dnssec/sub.sub.example.com/records
+mkdir ../bind_config_dnssec/sub.sub.example.com/records
+cp ./named.ca ../bind_config_dnssec/sub.sub.example.com/records
+
+rm -rf ../bind_config_dnssec/sub.sub.delay.com/records
+mkdir ../bind_config_dnssec/sub.sub.delay.com/records
+cp ./named.ca ../bind_config_dnssec/sub.sub.delay.com/records
+
 tree -if ../bind_config_dnssec/sub.sub.example.com/ |egrep 'named.conf' | xargs -I{} rm -rf {}
 tree -if ../bind_config_dnssec/sub.sub.delay.com/ |egrep 'named.conf' | xargs -I{} rm -rf {}
-tree -if ../bind_config_dnssec/sub.delay.com/records/ |egrep 'dsset|template|.key|.private' | xargs -I{} rm {}
+
 tree -if ../bind_config_dnssec/sub.example.com/ |egrep 'named.conf' | xargs -I{} rm -rf {}
 tree -if ../bind_config_dnssec/sub.delay.com/ |egrep 'named.conf' | xargs -I{} rm -rf {}
-tree -if ../bind_config_dnssec/example.com/records/ |egrep 'dsset|template|.key|.private' | xargs -I{} rm {}
+
+rm -rf ../bind_config_dnssec/example.com/records
+mkdir ../bind_config_dnssec/example.com/records
+cp ./named.ca ../bind_config_dnssec/example.com/records
+
 tree -if ../bind_config_dnssec/example.com/ |egrep 'named.conf' | xargs -I{} rm -rf {}
-tree -if ../bind_config_dnssec/com/records/ |egrep 'dsset|template|.key|.private' | xargs -I{} rm {}
+
+rm -rf ../bind_config_dnssec/com/records
+mkdir ../bind_config_dnssec/com/records
+cp ./named.ca ../bind_config_dnssec/com/records
+
 tree -if ../bind_config_dnssec/com/ |egrep 'named.conf' | xargs -I{} rm -rf {}
 tree -if ../bind_config_dnssec/root/ |egrep 'named.conf' | xargs -I{} rm -rf {}
