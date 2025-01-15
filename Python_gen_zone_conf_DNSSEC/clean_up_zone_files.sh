386e00f75a0c6aec9b2ee14a2da101bece42a384
Shigehiro Hattori <narihiro.h@gmail.com>
2025-01-15 10:54:20 +0900

    change egrep to grep -E

--- Python_gen_zone_conf_DNSSEC/clean_up_zone_files.sh
+++ Python_gen_zone_conf_DNSSEC/clean_up_zone_files.sh
@@ -8,8 +8,8 @@ rm -rf ../bind_config_dnssec/sub.sub.delay.com/records
 mkdir ../bind_config_dnssec/sub.sub.delay.com/records
 cp ./named.ca ../bind_config_dnssec/sub.sub.delay.com/records
 
-tree -if ../bind_config_dnssec/sub.sub.example.com/ |egrep 'named.conf' | xargs -I{} rm -rf {}
-tree -if ../bind_config_dnssec/sub.sub.delay.com/ |egrep 'named.conf' | xargs -I{} rm -rf {}
+tree -if ../bind_config_dnssec/sub.sub.example.com/ |grep -E  'named.conf' | xargs -I{} rm -rf {}
+tree -if ../bind_config_dnssec/sub.sub.delay.com/ |grep -E  'named.conf' | xargs -I{} rm -rf {}
 
 rm -rf ../bind_config_dnssec/sub.example.com/records
 mkdir ../bind_config_dnssec/sub.example.com/records
@@ -20,12 +20,12 @@ rm -rf ../bind_config_dnssec/example.com/records
 mkdir ../bind_config_dnssec/example.com/records
 cp ./named.ca ../bind_config_dnssec/example.com/records
 
-tree -if ../bind_config_dnssec/example.com/ |egrep 'named.conf' | xargs -I{} rm -rf {}
+tree -if ../bind_config_dnssec/example.com/ |grep -E  'named.conf' | xargs -I{} rm -rf {}
 
 rm -rf ../bind_config_dnssec/com/records
 mkdir ../bind_config_dnssec/com/records
 cp ./named.ca ../bind_config_dnssec/com/records
 
-tree -if ../bind_config_dnssec/com/ |egrep 'named.conf' | xargs -I{} rm -rf {}
-tree -if ../bind_config_dnssec/root/ |egrep 'named.conf' | xargs -I{} rm -rf {}
-tree -if ../bind_config_dnssec/root/records |egrep 'dsset|signed|key|private|root.db' | xargs -I{} rm -rf {}
+tree -if ../bind_config_dnssec/com/ |grep -E  'named.conf' | xargs -I{} rm -rf {}
+tree -if ../bind_config_dnssec/root/ |grep -E  'named.conf' | xargs -I{} rm -rf {}
+tree -if ../bind_config_dnssec/root/records |grep -E  'dsset|signed|key|private|root.db' | xargs -I{} rm -rf {}
