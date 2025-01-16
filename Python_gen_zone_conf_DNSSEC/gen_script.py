dc7f0bc768b3313a2402d1cffb94f91b2b834da1
Shigehiro Hattori <narihiro.h@gmail.com>
2025-01-16 20:08:30 +0900

    refactoring

--- Python_gen_zone_conf_DNSSEC/gen_script.py
+++ Python_gen_zone_conf_DNSSEC/gen_script.py
@@ -3,33 +3,44 @@
 import subprocess
 
 start = 0
-end = 100
-step = 10
-#end = 300000
-#step = 50000
+#end = 100
+#step = 10
+end = 300000
+step = 50000
+sleep_time = 1200
 
-with open('gen_dnssec_conf_01.sh', 'w') as f:
+with open('gen_dnssec_conf.sh', 'w') as f:
     f.write("#!/bin/sh\n\n")
+    f.write("# script01\n")
     for i in range(0, end, step):
-        str01 = f"python3 ./generate_sub.sub.example.com.py -s {i} -e {i+step} &"
-        str02 = f"python3 ./generate_sub.sub.delay.com.py -s {i} -e {i+step} &"
-        f.write(f"{str01}\n")
-        f.write(f"{str02}\n")
+        if i != end:
+            str01 = f"python3 ./generate_sub.sub.example.com.py -s {i} -e {i+step} &"
+            str02 = f"python3 ./generate_sub.sub.delay.com.py -s {i} -e {i+step} &"
+            f.write(f"{str01}\n")
+            f.write(f"{str02}\n")
+        #if i == 270000:
+        if i == (end - step):
+            str01 = f"python3 ./generate_sub.sub.example.com.py -s {i} -e {i+step} &"
+            str02 = f"python3 ./generate_sub.sub.delay.com.py -s {i} -e {i+step}"
+            f.write(f"{str01}\n")
+            f.write(f"{str02}\n")
+            f.write(f"sleep {sleep_time}\n")
 
-with open('gen_dnssec_conf_02.sh', 'w') as f:
-    f.write("#!/bin/sh\n\n")
+with open('gen_dnssec_conf.sh', 'a') as f:
+    f.write("\n# script02\n")
     for i in range(0, end, step):
-        str01 = f"python3 ./generate_sub.sub.delay.com_02.py -s {start} -e {end} &"
-        f.write(f"{str01}\n")
+        if i != end:
+            str01 = f"python3 ./generate_sub.sub.delay.com_02.py -s {start} -e {end} &"
+            f.write(f"{str01}\n")
+        if i == (end - step):
+            str01 = f"python3 ./generate_sub.sub.delay.com_02.py -s {start} -e {end}"
+            f.write(f"{str01}\n")
+            f.write(f"sleep {sleep_time}\n")
 
-with open('gen_dnssec_conf_03.sh', 'w') as f:
-    f.write("#!/bin/sh\n\n")
+with open('gen_dnssec_conf.sh', 'a') as f:
+    f.write("\n# script03\n")
     str01 = f"python3 ./generate_com_root_named.conf.py -s {start} -e {end}"
     f.write(f"{str01}\n")
 
-cmd01 = 'chmod +x ./gen_dnssec_conf_01.sh'
-cmd02 = 'chmod +x ./gen_dnssec_conf_02.sh'
-cmd03 = 'chmod +x ./gen_dnssec_conf_03.sh'
+cmd01 = 'chmod +x ./gen_dnssec_conf.sh'
 p = subprocess.run(cmd01.split(), capture_output=True, text=True)
-p = subprocess.run(cmd02.split(), capture_output=True, text=True)
-p = subprocess.run(cmd03.split(), capture_output=True, text=True)
